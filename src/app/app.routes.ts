5335a55d85bd14e410e5721662159618b0ad2329
harshani <ruwanthikapemarathna@gmail.com>
2024-07-09 15:21:25 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { ProductsComponent } from './components/products/products.component';
+import { OrdersComponent } from './components/orders/orders.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'/login', pathMatch:'full'},
+    {path:'login',component:LoginComponent},
+    {path:'dashboard',component:DashboardComponent},
+    {path:'customers',component:CustomersComponent},
+    {path:'products',component:ProductsComponent},
+    {path:'orders',component:OrdersComponent},
+];
