eed9d2133027af48728aeb7f1a2ad4747db6251d
harshani <ruwanthikapemarathna@gmail.com>
2024-07-09 23:13:56 +0530

    added customer component

--- src/app/components/customers/customers.component.ts
+++ src/app/components/customers/customers.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {MatTooltip} from "@angular/material/tooltip";
+import {CustomerStatusManagerComponent} from "./inner/customer-status-manager/customer-status-manager.component";
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon,
+    MatIconButton,
+    MatTooltip,
+    CustomerStatusManagerComponent
+  ],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.scss'
 })
