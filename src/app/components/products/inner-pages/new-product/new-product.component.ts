436737c26947d63dcb4312c1efca39156f07c2a9
harshani <ruwanthikapemarathna@gmail.com>
2024-07-17 23:03:06 +0530

    created new product component

---
+++ src/app/components/products/inner-pages/new-product/new-product.component.ts
@@ -0,0 +1,48 @@
+import { Component, inject } from '@angular/core';
+import {MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle} from "@angular/material/dialog";
+import {MatButton} from "@angular/material/button";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ProductService } from '../../../../service/product.service';
+
+@Component({
+  selector: 'app-new-product',
+  standalone: true,
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatButton,
+    MatDialogClose,
+    ReactiveFormsModule
+  ],
+  templateUrl: './new-product.component.html',
+  styleUrl: './new-product.component.scss'
+})
+export class NewProductComponent {
+  
+  readonly dialogRef = inject(MatDialogRef<NewProductComponent>);
+  readonly productService= inject(ProductService);
+
+  form = new FormGroup({
+    qty: new FormControl('',[Validators.required]),
+    unitPrice: new FormControl('',[Validators.required]),
+    description: new FormControl('',[Validators.required]),
+  });
+
+  create(){
+    const obj={
+      qty:this.form.value.qty,
+      unitPrice:this.form.value.unitPrice,
+      description:this.form.value.description
+    }
+    this.productService.create(obj).subscribe(response=>{
+      this.dialogRef.close(true);
+    }, error => {
+      console.log(error?.error?.message);
+    })
+  }
+  close(){
+    this.dialogRef.close(false);
+  }
+
+}
